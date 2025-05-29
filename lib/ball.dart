1232a2c09ff4b9b9300badf54c7f690087e3af32
tulsipatange9 <tulsipatange9@gmail.com>
2025-05-29 13:24:20 +0000

    Bucket App - Resume, quit, pause

---
+++ lib/ball.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Ball {
+  double x;
+  double y;
+  double speed;
+  Color color;
+  double size;
+
+  Ball({
+    required this.x,
+    required this.y,
+    required this.speed,
+    required this.color,
+    required this.size,
+  });
+}
